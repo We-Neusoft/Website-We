3c9e209130edb9c0f2b70d3118adeaffb10d7e5f
We Server <sfl@neusoft.edu.cn>
2012-07-29 20:41:42 +0800

    Homepage added timestamp

--- mirror/views.py
+++ mirror/views.py
@@ -1,14 +1,17 @@
 #coding=utf-8
 from django.shortcuts import render_to_response
+from os import stat
+from time import strftime, localtime
 
 def index(request):
    mirrors = ['apache', 'centos', 'cpan', 'cygwin', 'eclipse', 'epel', 'gentoo', 'putty', 'pypi', 'ubuntu', 'ubuntu-release']
    results = []
    for mirror in mirrors:
-      result_file = open('/storage/mirror/.' + mirror, 'r')
+      filename = '/storage/mirror/.' + mirror
+      result_file = open(filename, 'r')
       result = result_file.readline()[:-1]
       if result == '-1':
-         result = '同步中...'
+         result = '正在同步'
          style = 'we-mirror-syncing'
       elif result == '0':
          result = '同步成功'
@@ -17,5 +20,6 @@ def index(request):
          result = '同步失败'
          style = 'we-mirror-sync-faild'
       result_file.close()
-      results.append({'mirror': mirror, 'status': result, 'style': style})
+      lasttime = stat(filename).st_mtime
+      results.append({'mirror': mirror, 'status': result, 'style': style, 'timestamp': strftime('%Y-%m-%d %H:%M:%S', localtime(lasttime))})
    return render_to_response('mirror/index.html', {'results': results})
