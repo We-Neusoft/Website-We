bfe7ad2a7e843583494b1e7701d43d89d376e2a7
We Server <sfl@neusoft.edu.cn>
2012-07-29 22:15:04 +0800

    Show last sync success time on mirror site homepage

--- mirror/views.py
+++ mirror/views.py
@@ -1,25 +1,21 @@
 #coding=utf-8
 from django.shortcuts import render_to_response
-from os import stat
-from time import strftime, localtime
 
 def index(request):
    mirrors = ['apache', 'centos', 'cpan', 'cygwin', 'eclipse', 'epel', 'gentoo', 'putty', 'pypi', 'ubuntu', 'ubuntu-release']
    results = []
    for mirror in mirrors:
-      filename = '/storage/mirror/.' + mirror
-      result_file = open(filename, 'r')
-      result = result_file.readline()[:-1]
-      if result == '-1':
-         result = '正在同步'
+      pathname = '/storage/mirror/'
+      status = open(pathname + '.' + mirror + '.status').readline()[:-1]
+      if status == '-1':
+         status = '正在同步'
          style = 'we-mirror-syncing'
-      elif result == '0':
-         result = '同步成功'
+      elif status == '0':
+         status = '同步成功'
          style = 'we-mirror-sync-success'
       else:
-         result = '同步失败'
+         status = '同步失败'
          style = 'we-mirror-sync-faild'
-      result_file.close()
-      lasttime = stat(filename).st_mtime
-      results.append({'mirror': mirror, 'status': result, 'style': style, 'timestamp': strftime('%Y-%m-%d %H:%M:%S', localtime(lasttime))})
+      timestamp = open(pathname + '.' + mirror + '.timestamp').readline()[:-1]
+      results.append({'mirror': mirror, 'status': status, 'style': style, 'timestamp': timestamp})
    return render_to_response('mirror/index.html', {'results': results})
