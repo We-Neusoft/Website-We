f3be616d3056918cead15b94945a684d93445d6d
We Server <sfl@neusoft.edu.cn>
2012-07-29 15:59:01 +0800

    Mirror site template

--- mirror/views.py
+++ mirror/views.py
@@ -2,4 +2,17 @@
 from django.shortcuts import render_to_response
 
 def index(request):
-   return render_to_response('mirror/index.html', {})
+   mirrors = ['apache', 'centos', 'cpan', 'cygwin', 'eclipse', 'epel', 'gentoo', 'putty', 'putty', 'pypi', 'ubuntu', 'ubuntu-release']
+   results = []
+   for mirror in mirrors:
+      result_file = open('/storage/mirror/.' + mirror, 'r')
+      result = result_file.read()
+      if result < 0:
+         result = '同步中...'
+      elif result == 0:
+         result = '同步成功'
+      else:
+         result = '同步失败'
+      result_file.close()
+      results.append({'mirror': mirror, 'status': result})
+   return render_to_response('mirror/index.html', {'results': results})
