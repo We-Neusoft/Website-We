0539519716d62956fe11a284eabb385e0dda9bed
Sun Fulong <sfl@neusoft.edu.cn>
2012-09-11 20:57:46 +0800

    镜像站添加两列

--- django/mirror/views.py
+++ django/mirror/views.py
@@ -17,5 +17,7 @@ def index(request):
          status = '同步失败'
          style = 'error'
       timestamp = open(pathname + '.' + mirror + '.timestamp').readline()[:-1]
-      results.append({'mirror': mirror, 'status': status, 'style': style, 'timestamp': timestamp})
-   return render_to_response('mirror/index.html', {'results': results})
+      count = open(pathname + '.' + mirror + '.count').readline()[:-1]
+      size = open(pathname + '.' + mirror + '.size').readline()[:-1]
+      results.append({'mirror': mirror, 'status': status, 'style': style, 'timestamp': timestamp, 'count': count, 'size': size})
+   return render_to_response('mirror/index.weml', {'results': results})
