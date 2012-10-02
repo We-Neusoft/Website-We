2f57759b0d227f2de7e0c18ea04e5fc20f5c5a25
We Server <sfl@neusoft.edu.cn>
2012-10-02 09:08:09 +0800

    Init dev branch.

--- django/settings.py
+++ django/settings.py
@@ -100,13 +100,13 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'we.urls'
+ROOT_URLCONF = 'dev.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/data/web',
+    '/data/web/dev',
 )
 
 INSTALLED_APPS = (
