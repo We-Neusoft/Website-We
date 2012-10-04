e582cffdf0c51cf2b35ed5fec7e7953802d78ba3
We Server <sfl@neusoft.edu.cn>
2012-10-04 16:18:18 +0800

    Download app created.

--- django/settings.py
+++ django/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'mirror',
+    'download',
 )
 
 # A sample logging configuration. The only tangible logging
