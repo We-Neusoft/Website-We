b3d547d6e492ac5be678350964d50ee92d0d2f3e
Sun Fulong <sfl@neusoft.edu.cn>
2012-10-05 09:20:59 +0800

    目录结构调整

--- dynamic/settings.py
+++ dynamic/settings.py
@@ -1,6 +1,6 @@
 # Django settings for we project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -100,13 +100,13 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'dev.urls'
+ROOT_URLCONF = 'dynamic.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/data/web/dev',
+    '/data/publish/template',
 )
 
 INSTALLED_APPS = (
