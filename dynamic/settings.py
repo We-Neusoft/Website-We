013c22af6914d5430ad7bd744539006f5130151a
Sun Fulong <sfl@neusoft.edu.cn>
2012-10-05 09:38:58 +0800

    Django配置文件调整

--- dynamic/settings.py
+++ dynamic/settings.py
@@ -1,6 +1,6 @@
 # Django settings for we project.
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -106,7 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/data/publish/template',
+    '/data/develop/template',
 )
 
 INSTALLED_APPS = (
