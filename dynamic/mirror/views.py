ade1797403c1194fe2f209837def16de1ad6f282
Sun Fulong <sfl@neusoft.edu.cn>
2012-10-19 22:41:20 +0800

    镜像站CPAN调整为实时同步

--- dynamic/mirror/views.py
+++ dynamic/mirror/views.py
@@ -1,4 +1,7 @@
 #coding=utf-8
+import json
+import time
+
 from django.shortcuts import render_to_response
 from common.unit import file_size
 
@@ -7,18 +10,23 @@ def index(request):
    results = []
    for mirror in mirrors:
       pathname = '/storage/mirror/'
-      status = open(pathname + '.' + mirror + '.status').readline()[:-1]
-      if status == '-1':
-         status = '正在同步'
-         style = 'info'
-      elif status == '0':
-         status = '同步成功'
+      if mirror == 'cpan':
+         status = '实时同步'
          style = 'success'
+         timestamp = time.strftime('%Y-%m-%d %H-%M-%S %Z', time.localtime(json.loads(open(pathname + 'cpan/RECENT-1h.json').readline())[u'meta'][u'Producers'][u'time']))
       else:
-         status = '同步失败'
-         style = 'error'
+         status = open(pathname + '.' + mirror + '.status').readline()[:-1]
+         if status == '-1':
+            status = '正在同步'
+            style = 'info'
+         elif status == '0':
+            status = '同步成功'
+            style = 'success'
+         else:
+            status = '同步失败'
+            style = 'error'
+         timestamp = open(pathname + '.' + mirror + '.timestamp').readline()[:-1]
       count = open(pathname + '.' + mirror + '.count').readline()[:-1]
       size = open(pathname + '.' + mirror + '.size').readline()[:-1]
-      timestamp = open(pathname + '.' + mirror + '.timestamp').readline()[:-1]
       results.append({'mirror': mirror, 'status': status, 'style': style, 'count': count, 'size': file_size(int(size)), 'timestamp': timestamp})
    return render_to_response('mirror/index.weml', {'results': results})
