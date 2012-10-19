b5f4e7f1c89c1d1378c1be1d8ef7201b70463e90
Sun Fulong <sfl@neusoft.edu.cn>
2012-10-19 22:41:20 +0800

    镜像站CPAN调整为实时同步

--- dynamic/mirror/views.py
+++ dynamic/mirror/views.py
@@ -20,6 +20,7 @@ def index(request):
       if mirror == 'cpan':
          status = '实时同步'
          style = 'success'
+         timestamp = time.strftime('%Y-%m-%d %H-%M-%S %Z', time.localtime(json.loads(open(pathname + 'cpan/RECENT-1h.json').readline())[u'meta'][u'Producers'][u'time']))
       else:
          status = open(pathname + '.' + mirror + '.status').readline()[:-1]
          if status == '-1':
