7eb00c8fd8c32c1957570af19d4d2770e8dc8ee5
We Server <sfl@neusoft.edu.cn>
2012-10-20 11:46:17 +0800

    Mirror site change some timestamp source.

--- dynamic/mirror/views.py
+++ dynamic/mirror/views.py
@@ -5,15 +5,21 @@ import time
 from django.shortcuts import render_to_response
 from common.unit import file_size
 
+def timestamp_to_localtime(timestamp): 
+   return time.strftime('%Y-%m-%d %H:%M:%S %Z', time.localtime(timestamp))
+
+def timestring_to_localtime(timestring): 
+   return time.strftime('%Y-%m-%d %H:%M:%S %Z', time.strptime(timestring, '%a %b %d %H:%M:%S %Z %Y'))
+
 def index(request):
+   pathname = '/storage/mirror/'
    mirrors = ['centos', 'epel', 'repoforge', 'ubuntu', 'ubuntu-releases', 'archlinux', 'gentoo', 'gentoo-portage', 'cpan', 'apache', 'cygwin', 'eclipse', 'mozilla-current', 'putty']
    results = []
+
    for mirror in mirrors:
-      pathname = '/storage/mirror/'
       if mirror == 'cpan':
          status = '实时同步'
          style = 'success'
-         timestamp = time.strftime('%Y-%m-%d %H-%M-%S %Z', time.localtime(json.loads(open(pathname + 'cpan/RECENT-1h.json').readline())[u'meta'][u'Producers'][u'time']))
       else:
          status = open(pathname + '.' + mirror + '.status').readline()[:-1]
          if status == '-1':
@@ -25,8 +31,31 @@ def index(request):
          else:
             status = '同步失败'
             style = 'error'
-         timestamp = open(pathname + '.' + mirror + '.timestamp').readline()[:-1]
+
       count = open(pathname + '.' + mirror + '.count').readline()[:-1]
       size = open(pathname + '.' + mirror + '.size').readline()[:-1]
+
+      if mirror == 'centos':
+         timestamp = timestamp_to_localtime(int(open(pathname + mirror + '/TIME').readline()))
+      elif mirror == 'cpan':
+         timestamp = timestamp_to_localtime(json.loads(open(pathname + mirror + '/RECENT-1h.json').readline())[u'meta'][u'Producers'][u'time'])
+      elif mirror == 'repoforge':
+         timestamp = timestamp_to_localtime(int(open(pathname + mirror + '/TIME').readline()))
+      elif mirror == 'ubuntu':
+         timestamp = timestring_to_localtime(open(pathname + mirror + '/project/trace/sadashbia.canonical.com').readline()[:-1])
+      elif mirror == 'archlinux':
+         timestamp = timestamp_to_localtime(int(open(pathname + mirror + '/lastsync').readline()))
+      elif mirror == 'gentoo':
+         timestamp = timestamp_to_localtime(int(open(pathname + mirror + '/experimental/timestamp.mirmon').readline()))
+      elif mirror == 'apache':
+         timestamp = timestamp_to_localtime(int(open(pathname + mirror + '/zzz/time.txt').readline()))
+      elif mirror == 'eclipse':
+         timestamp = timestamp_to_localtime(int(open(pathname + mirror + '/TIME').readline()))
+#      elif mirror == 'mozilla-current':
+#         timestamp = timestring_to_localtime(open(pathname + mirror + '/zz/marker.txt').readline()[:-1])
+      else:
+         timestamp = open(pathname + '.' + mirror + '.timestamp').readline()[:-1]
+
       results.append({'mirror': mirror, 'status': status, 'style': style, 'count': count, 'size': file_size(int(size)), 'timestamp': timestamp})
+
    return render_to_response('mirror/index.weml', {'results': results})
