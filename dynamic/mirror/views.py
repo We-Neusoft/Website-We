f13c3fba158cdedf84307f233efcc7906461a53e
Sun Fulong <sfl@neusoft.edu.cn>
2013-04-27 20:44:59 +0800

    镜像站添加ldp

--- dynamic/mirror/views.py
+++ dynamic/mirror/views.py
@@ -26,7 +26,7 @@ def get_value(mirror, key, time=0):
 
 @cache_page(60, key_prefix="mirror")
 def index(request):
-   mirrors = ['centos', 'epel', 'repoforge', 'kali', 'kali-security', 'kali-images', 'linuxmint', 'linuxmint-releases', 'raspbian', 'ubuntu-releases', 'archlinux', 'gentoo', 'gentoo-portage', 'cpan', 'pypi', 'cygwin', 'eclipse', 'putty', 'android', 'qt']
+   mirrors = ['centos', 'epel', 'repoforge', 'kali', 'kali-security', 'kali-images', 'linuxmint', 'linuxmint-releases', 'raspbian', 'ubuntu-releases', 'archlinux', 'gentoo', 'gentoo-portage', 'cpan', 'pypi', 'cygwin', 'eclipse', 'putty', 'android', 'qt', 'ldp']
    results = []
 
    for mirror in mirrors:
