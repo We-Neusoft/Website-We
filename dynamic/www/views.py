a2322410b94c168459a7d1f9c0d26858f660190d
We Server <sfl@neusoft.edu.cn>
2013-10-05 16:50:25 +0800

    Remove old directories.

--- dynamic/www/views.py
+++
@@ -1,9 +0,0 @@
-#coding=utf-8
-
-from django.shortcuts import render_to_response
-
-def index(request):
-   return render_to_response('www/index.weml', {'nav_www': 'active'});
-
-def more_services(request):
-   return render_to_response('www/more_services.weml', {'nav_www': 'active'});
