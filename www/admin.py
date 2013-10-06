98bd51de8faf432fc6209dd4a1897d30b5b53fe9
We Server <sfl@neusoft.edu.cn>
2013-10-06 16:26:44 +0800

    Added database support.

---
+++ www/admin.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+from django.contrib import admin
+from www.models import MoreService
+
+class MoreServiceAdmin(admin.ModelAdmin):
+    list_display = ('order', 'key', 'title', 'subtitle', 'modified')
+    fieldsets = [
+        ('标识', {'fields': ['key', 'order']}),
+        ('标题', {'fields': ['title', 'subtitle']}),
+        ('内容', {'fields': ['content']}),
+    ]
+
+admin.site.register(MoreService, MoreServiceAdmin)
