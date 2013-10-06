82bda8f4ad6cbeb383d656f3eb3b97ca126b49d6
We Server <sfl@neusoft.edu.cn>
2013-10-06 16:25:56 +0800

    Added database support.

--- www/models.py
+++ www/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class MoreService(models.Model):
+    key = models.CharField(max_length=16)
+    order = models.IntegerField()
+    title = models.CharField(max_length=64)
+    subtitle = models.CharField(max_length=64)
+    content = models.TextField()
+    modified = models.DateTimeField(auto_now=True)
