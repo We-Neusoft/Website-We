8ab0c8977d425f85e40372b67da2795e201b4239
We Server <sfl@neusoft.edu.cn>
2013-10-10 17:59:28 +0800

    Added human readable name for model fields.

--- www/models.py
+++ www/models.py
@@ -3,11 +3,16 @@ from django.db import models
 
 # 更多服务
 class MoreService(models.Model):
-    key = models.CharField(max_length=16)
-    order = models.IntegerField()
-    title = models.CharField(max_length=64)
-    subtitle = models.CharField(max_length=64)
-    content = models.TextField()
-    intranet = models.BooleanField()
-    internet = models.BooleanField()
-    modified = models.DateTimeField(auto_now=True)
+    key = models.CharField('标识', max_length=16)
+    order = models.IntegerField('排序')
+    title = models.CharField('标题', max_length=64)
+    subtitle = models.CharField('副标题', max_length=64)
+    content = models.TextField('正文')
+    intranet = models.BooleanField('校园网')
+    internet = models.BooleanField('互联网')
+    modified = models.DateTimeField('最后修改', auto_now=True)
+
+    class Meta:
+        ordering = ['order']
+        verbose_name = '更多服务'
+        verbose_name_plural = '更多服务'
