d7dac7e7b6a785684424fe38cf2e2c4ef6c50f0c
We Server <sfl@neusoft.edu.cn>
2013-10-11 22:36:13 +0800

    Fix url handle bugs.

--- mirror/urls.py
+++ mirror/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import patterns
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     (r'^$', 'mirror.views.index'),
     (r'^index.we$', 'mirror.views.index'),
-    (r'^configurations.we$', 'mirror.views.configurations'),
+    url(r'^configurations.we$', 'mirror.views.configurations', name='configurations'),
 )
