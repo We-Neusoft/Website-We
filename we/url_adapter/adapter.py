ea5022283e83acfc533528bdd8d214a899ce08e6
We Server <sfl@neusoft.edu.cn>
2013-10-13 07:13:34 +0800

    URL Adapter add form support.

--- we/url_adapter/adapter.py
+++ we/url_adapter/adapter.py
@@ -11,15 +11,24 @@ class AdapterMiddleware(object):
         if DEBUG_ENABLED:
             return response
 
-        response.content = re.sub(r'<a href="/([^/]+)/([^"]*)">', self.handle_href, response.content)
+        response.content = re.sub(r'<a href="/([^/]+)/([^"]*)">', self.handle_a, response.content)
+        response.content = re.sub(r'<form(([^>]*)action="/([^/]+)/([^"]*)"([^>]*))>', self.handle_form, response.content)
 
         return response
 
-    def handle_href(self, match):
+    def handle_a(self, match):
         target_app = match.group(1)
         target_url = match.group(2)
 
         return '<a href="' + self.handle_url(target_app, target_url) + '">'
+
+    def handle_form(self, match):
+        target_app = match.group(3)
+        target_url = match.group(4)
+        property_pre = match.group(2)
+        property_suf = match.group(5)
+
+        return '<form' + property_pre + 'action="' + self.handle_url(target_app, target_url) + '"' + property_suf + '>'
 
     def handle_url(self, target_app, target_url):
         if target_app == self.app:
