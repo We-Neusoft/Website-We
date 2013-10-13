87ef6d45eefee961cb060ba88195ceff17bb1c6f
We Server <sfl@neusoft.edu.cn>
2013-10-13 10:43:45 +0800

    URL Adapter added link and script support.

--- we/url_adapter/adapter.py
+++ we/url_adapter/adapter.py
@@ -8,11 +8,13 @@ class AdapterMiddleware(object):
     def process_response(self, request, response):
         self.app = re.match(r'/([^/]+)/', request.path).group(1)
 
-        if DEBUG_ENABLED:
-            return response
+#        if DEBUG_ENABLED:
+#            return response
 
         response.content = re.sub(r'<a href="/([^/]+)/([^"]*)">', self.handle_a, response.content)
-        response.content = re.sub(r'<form(([^>]*)action="/([^/]+)/([^"]*)"([^>]*))>', self.handle_form, response.content)
+        response.content = re.sub(r'<form([^>]*)action="/([^/]+)/([^"]*)"([^>]*)>', self.handle_form, response.content)
+        response.content = re.sub(r'<link([^>]*)href="/static/([^"]*)"([^>]*)>', self.handle_link, response.content)
+        response.content = re.sub(r'<script([^>]*)src="/static/([^"]*)"([^>]*)>', self.handle_script, response.content)
 
         return response
 
@@ -23,12 +25,26 @@ class AdapterMiddleware(object):
         return '<a href="' + self.handle_url(target_app, target_url) + '">'
 
     def handle_form(self, match):
-        target_app = match.group(3)
-        target_url = match.group(4)
-        property_pre = match.group(2)
-        property_suf = match.group(5)
+        target_app = match.group(2)
+        target_url = match.group(3)
+        property_pre = match.group(1)
+        property_suf = match.group(4)
 
         return '<form' + property_pre + 'action="' + self.handle_url(target_app, target_url) + '"' + property_suf + '>'
+
+    def handle_link(self, match):
+        target_url = match.group(2)
+        property_pre = match.group(1)
+        property_suf = match.group(3)
+
+        return '<link' + property_pre + 'href="' + '/' + target_url + '"' + property_suf + '>'
+
+    def handle_script(self, match):
+        target_url = match.group(2)
+        property_pre = match.group(1)
+        property_suf = match.group(3)
+
+        return '<script' + property_pre + 'src="' + '/' + target_url + '"' + property_suf + '>'
 
     def handle_url(self, target_app, target_url):
         if target_app == self.app:
