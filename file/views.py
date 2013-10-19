2c0da6f8b324e7c65365af8383680f90dde10f85
We Server <sfl@neusoft.edu.cn>
2013-10-19 08:50:21 +0800

    Using StreamingHttpResponse handle file download.

--- file/views.py
+++ file/views.py
@@ -1,6 +1,10 @@
+from django.http import StreamingHttpResponse
 from django.views import generic
 
 from file.models import File
 
 class FileView(generic.DetailView):
     model = File
+
+def download(request, id):
+    return StreamingHttpResponse(id, content_type='application/octet-stream')
