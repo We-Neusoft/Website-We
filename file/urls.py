2247fa7bb2a5d4116c5a9a1bf6cf3cc50092d4e5
We Server <sfl@neusoft.edu.cn>
2013-10-19 08:50:21 +0800

    Using StreamingHttpResponse handle file download.

--- file/urls.py
+++ file/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from file import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<pk>[^\.]+).we$', views.FileView.as_view(), name='file'),
+    url(r'^file/(?P<pk>[^\.]+).we$', views.FileView.as_view(), name='file'),
+    url(r'^download/(?P<id>[^\.]+).we$', views.download, name='download'),
 )
