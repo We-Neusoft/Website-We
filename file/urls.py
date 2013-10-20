46be193a0d5ea07adbe2520c459903fab4906119
We Server <sfl@neusoft.edu.cn>
2013-10-21 07:14:32 +0800

    File app added home page.

--- file/urls.py
+++ file/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import patterns, url
 from file import views
 
 urlpatterns = patterns('',
-    url(r'^detail/(?P<pk>[^\.]+).we$', views.FileView.as_view(), name='file'),
+    url(r'^$', views.index, name='index'),
+    (r'^index.we$', views.index),
+    url(r'^detail/(?P<pk>[^\.]+).we$', views.FileView.as_view(), name='detail'),
     url(r'^download/(?P<id>[^\.]+).we$', views.download, name='download'),
 )
