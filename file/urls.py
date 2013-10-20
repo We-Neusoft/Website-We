b19b525549159398da78ab575edc636bf798fafe
We Server <sfl@neusoft.edu.cn>
2013-10-21 07:21:26 +0800

    Change file app url style.

--- file/urls.py
+++ file/urls.py
@@ -5,6 +5,6 @@ from file import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     (r'^index.we$', views.index),
-    url(r'^detail/(?P<pk>[^\.]+).we$', views.FileView.as_view(), name='detail'),
-    url(r'^download/(?P<id>[^\.]+).we$', views.download, name='download'),
+    url(r'^(?P<pk>[\w\-]+).we$', views.FileView.as_view(), name='detail'),
+    url(r'^(?P<id>[\w\-]+)/download.we$', views.download, name='download'),
 )
