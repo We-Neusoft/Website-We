97a6a20d7080df06d624d2a16fdc68d045e84295
We Server <sfl@neusoft.edu.cn>
2013-11-09 07:47:57 +0800

    Mailbox auth added domain validate.

--- we/auth/mailbox.py
+++ we/auth/mailbox.py
@@ -2,16 +2,21 @@ from django.contrib.auth.models import User
 
 from imaplib import IMAP4
 
+ALLOWED_DOMAIN = ['nou.com.cn', 'neusoft.edu.cn']
+
 class MailboxAuth(object):
     def authenticate(self, username=None, domain=None, password=None):
+        if not domain.lower() in ALLOWED_DOMAIN:
+            return None
+
         try:
-            mailbox = IMAP4('mail.' + domain[1:])
-            mailbox.login(username + domain, password)
+            mailbox = IMAP4('mail.' + domain)
+            mailbox.login(username + '@' + domain, password)
 
             try:
-                user = User.objects.get(username=username + domain)
+                user = User.objects.get(username=username + '@' + domain)
             except User.DoesNotExist:
-                user = User(username=username + domain, email=username + domain)
+                user = User(username=username + '@' + domain, email=username + '@' + domain)
                 user.save()
 
             return user
