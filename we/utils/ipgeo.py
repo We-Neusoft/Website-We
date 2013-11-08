9f1b5347368a75e22990f0f92db202d3cef74db7
We Server <sfl@neusoft.edu.cn>
2013-11-09 07:47:57 +0800

    Mailbox auth added domain validate.

--- we/utils/ipgeo.py
+++ we/utils/ipgeo.py
@@ -3,12 +3,14 @@
 from netaddr import IPAddress, IPNetwork
 
 ip_network = [
+    IPNetwork('127.0.0.1/32'),
     IPNetwork('172.18.0.0/16'), IPNetwork('172.19.0.0/17'), IPNetwork('172.21.0.0/17'),
     IPNetwork('172.22.0.0/16'), IPNetwork('172.23.0.0/16'), IPNetwork('172.24.0.0/16'),
     IPNetwork('192.168.24.0/24'), IPNetwork('192.168.102.0/24'),
     IPNetwork('219.216.128.0/24'), IPNetwork('219.216.129.0/27'),
 ]
 ip_name = [
+    'localhost',
     'wireless', 'apartment', 'unicom',
     'classroom', 'faculty', 'server_172',
     'administration', 'server_192',
