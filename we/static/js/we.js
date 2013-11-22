e8727fc4a159ea5315dbe0cc7d5864cfc29cc3e4
We Server <sfl@neusoft.edu.cn>
2013-11-22 08:35:41 +0800

    Add dropdown visibility control.

---
+++ we/static/js/we.js
@@ -0,0 +1,8 @@
+'use strict';
+
+$(document).ready(function () {
+    $.getJSON('/common/get_user.we', function(data) {
+        $('#me').html(data.username + '，你好！');
+        $('.dropdown').css('visibility', 'visible');
+    });
+});
