6cdc068b7848d9fd6a032365e682c61e2779b909
We Server <sfl@neusoft.edu.cn>
2013-11-23 08:33:14 +0800

    Formatter js to hit jslint test.

--- we/static/js/we.js
+++ we/static/js/we.js
@@ -1,7 +1,7 @@
 'use strict';
 
 $(document).ready(function () {
-    $.getJSON('/common/get_user.we', function(data) {
+    $.getJSON('/common/get_user.we', function (data) {
         $('#me').html(data.username + '，你好！');
         $('.dropdown').css('visibility', 'visible');
     });
