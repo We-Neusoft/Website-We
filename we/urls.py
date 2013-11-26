45b0d3602c38bff9a57f00ffa10363e828572127
We Server <sfl@neusoft.edu.cn>
2013-11-26 08:30:33 +0800

    Added opacity to navbar.

--- we/urls.py
+++ we/urls.py
@@ -1,13 +1,12 @@
-from django.conf.urls import patterns, include
+from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'we.views.home', name='home'),
-    # url(r'^we/', include('we.foo.urls')),
+    # url(r'^blog/', include('blog.urls')),
     (r'^([^/]+/)?common/', include('common.urls')),
     (r'^www/', include('www.urls', namespace='www')),
     (r'^mirror/', include('mirror.urls', namespace='mirror')),
@@ -15,9 +14,6 @@ urlpatterns = patterns('',
     (r'^file/', include('file.urls', namespace='file')),
     (r'^me/', include('me.urls', namespace='me')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^jsreverse/$', 'django_js_reverse.views.urls_js', name='js_reverse'),
     (r'^admin/', include(admin.site.urls)),
 )
