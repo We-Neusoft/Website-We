fcabc716d2d5def4dbefa8e04202a9dae116c1bd
We Server <sfl@neusoft.edu.cn>
2013-12-31 08:53:36 +0800

    Adding client id verification.

--- api/oauth/views.py
+++ api/oauth/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User, Group
 from django.http import HttpResponse, HttpResponseRedirect
 
 def authorize(request):
@@ -5,7 +6,13 @@ def authorize(request):
         response_type = request.REQUEST['response_type']
         client_id = request.REQUEST['client_id']
     except KeyError:
-        return HttpResponse('invalid_request')
+        return HttpResponse('error:invalid_request')
+
+    try:
+        client = User.objects.get(username=client_id)
+        client.groups.get(name='oauth')
+    except (User.DoesNotExist, Group.DoesNotExist):
+        return HttpResponse('Invalid client id.')
 
     redirect_uri = request.REQUEST.get('redirect_uri', '')
     scope = request.REQUEST.get('scope', '')
