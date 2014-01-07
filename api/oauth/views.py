4355eff164d07a1c6bf694a77a0bdec0abe1dcfe
We Server <sfl@neusoft.edu.cn>
2014-01-07 08:47:26 +0800

    Adding some comments in oauth.

--- api/oauth/views.py
+++ api/oauth/views.py
@@ -1,3 +1,4 @@
+#coding=utf8
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.core.context_processors import csrf
@@ -10,9 +11,13 @@ from api.oauth.models import AuthorizationCode, Client, RedirectionUri
 from api.oauth.forms import AuthenticationForm, InitializationForm
 
 def authorize(request):
+    # 验证是否为登录表单
+    # TODO 当系统有初始化、登录以外的入口时，此处需要改进（SunFulong@2014-1-7）
     form = AuthenticationForm(request.POST)
 
+    # 不是登录表单，返回登录画面
     if not form.is_valid():
+        # 验证应用端身份
         form, client = verify_client(request.REQUEST)
         if issubclass(form.__class__, HttpResponse):
              return form
@@ -24,11 +29,13 @@ def authorize(request):
         request.session.update(form.cleaned_data)
 
         return render_to_response('api/oauth/authorize.html', csrf(request))
+    # 是登录表单，进行相关验证
     else:
         action = form.cleaned_data['action']
         username = form.cleaned_data['username']
         password = form.cleaned_data['password']
 
+        # 验证应用端身份
         form, client = verify_client(request.REQUEST)
         if issubclass(form.__class__, HttpResponse):
             return form
@@ -39,46 +46,60 @@ def authorize(request):
         scope = request.session['scope']
         state = request.session['state']
 
+        # 处理code请求
         if response_type == 'code':
+            # 处理登录以外的请求
             if not action == 'Submit':
                 return callback_client(redirect_uri + '?error=access_denied', state)
 
+            # 验证表单合法性
+            if not username or not password:
+                return HttpResponse('Missing username and/or password!')
+
+            # 验证用户合法性
             user = authenticate(email=username, password=password)
-            if user:
-                code = AuthorizationCode(client=client, user=user, redirect_uri=redirect_uri, expire_time=datetime.datetime.now() + datetime.timedelta(minutes=10))
-                code.save()
+            if not user:
+                return HttpResponse('Failure')
 
-                return callback_client(redirect_uri + '?code=' + code.code.encode(), state)
+            # 生成code
+            code = AuthorizationCode(client=client, user=user, redirect_uri=redirect_uri, expire_time=datetime.datetime.now() + datetime.timedelta(minutes=10))
+            code.save()
 
-            return HttpResponse('Failure')
+            return callback_client(redirect_uri + '?code=' + code.code.encode(), state)
+
         else:
             return callback_client(redirect_uri + '?error=unsupported_response_type', state), None
 
 def token(request):
+    # 获得应用端验证信息
     basic = request.META.get('HTTP_AUTHORIZATION')
     if not basic:
         response = HttpResponse('401 Unauthorized', status=401)
         response['WWW-Authenticate'] = 'Basic realm="Please provide your client_id and client_secret."'
         return response
 
+    # 验证应用端合法性
     client_id, client_secret = basic[6:].decode('base64').split(':')
     client = Client.objects.get(client_id=client_id, client_secret=client_secret)
 
     return HttpResponse(client_id + '-' + client_secret)
 
 def verify_client(form):
+    # 验证请求合法性
     form = InitializationForm(form)
     if not form.is_valid():
         return HttpResponse('Invalid_request'), None
 
     state = form.cleaned_data['state']
 
+    # 验证应用端身份
     client_id = form.cleaned_data['client_id']
     try:
         client = Client.objects.get(client_id=client_id)
     except (Client.DoesNotExist):
         return HttpResponse('Invalid client id.'), None
 
+    # 验证重定向URI合法性
     redirect_uri = form.cleaned_data['redirect_uri']
     try:
         RedirectionUri.objects.filter(client=client).get(redirect_uri=redirect_uri)
