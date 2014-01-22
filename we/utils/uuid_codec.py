14cb16b3e3a0bd5d55fb5a6155bc32b34d6b03be
We Server <sfl@neusoft.edu.cn>
2014-01-22 08:38:36 +0800

    Move uuid codec util to btt codec lib.

--- we/utils/uuid_codec.py
+++
@@ -1,8 +0,0 @@
-from base64 import urlsafe_b64encode, urlsafe_b64decode
-from uuid import UUID
-
-def encode(uuid):
-    return base64.urlsafe_b64encode(uuid.bytes.encode('base64')).rstrip('=')
-
-def decode(string):
-    return UUID(bytes=urlsafe_b64decode(string + '=='))
